522449bf46d8557808c9e1490cfdb2e4f086f8b7
Joe Henriod <webdev@themobmuseum.org>
2022-08-05 16:40:41 -0700

    Facebook Fix ?
    We will see?

--- script.js
+++ script.js
@@ -272,7 +272,8 @@ function genShareToLink(){
     NGMailToLink.href = theMAilToLink
 
     // Facebook Link
-
+    // Probably don't need this - Test MOnday facebookPath = window.location.pathname
+    document.getElementById("fb-share-button").setAttribute("data-href", getTheFullURL);
 
 }
 
